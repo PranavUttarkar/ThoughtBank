0083c8cd85932be1b745ceadf6ee69dedbb46843
PranavUttarkar <pranavuttarkar6@gmail.com>
2025-03-22 11:28:21 -0500

    Log In with Firebase Added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,15 @@ import axios from "axios";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Box, Slider } from "@chakra-ui/react";
+import { MdGraphicEq } from "react-icons/md";
+import { Auth } from "./components/auth";
 
 function App() {
   const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
+
+  //---------- Python Server Interaction ---------------------//
 
   const fetchAPI = async () => {
     const response = await axios.get("http://127.0.0.1:8008/api/users");
@@ -16,31 +21,45 @@ function App() {
 
   useEffect(() => {
     fetchAPI();
+
+    //------------------------------------------------//
   }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* FIRE BASE TUTORIAL */}
+      <div className="App">
+        <Auth />
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         {array.map((user, index) => (
           <div key={index}>
             <span key={index}>{user}</span>
           </div>
         ))}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Slider.Root maxW="1200px" defaultValue={[50]}>
+        <Slider.Control>
+          <Slider.Track bg="red.100">
+            <Slider.Range bg="tomato" />
+          </Slider.Track>
+          <Slider.Thumb index={0} boxSize={6} borderColor="tomato" shadow="md">
+            <Box color="tomato" as={MdGraphicEq} />
+            <Slider.DraggingIndicator
+              bg="red.100"
+              layerStyle="fill.solid"
+              top="6"
+              rounded="sm"
+              px="1.5"
+            >
+              <Slider.ValueText />
+            </Slider.DraggingIndicator>
+          </Slider.Thumb>
+        </Slider.Control>
+      </Slider.Root>
     </>
   );
 }
---
+++ client/src/config/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNoxEbv6sdfaiPwmdc8O8WVvQW3RAFaQo",
+  authDomain: "thoughtbank-original-set-up.firebaseapp.com",
+  projectId: "thoughtbank-original-set-up",
+  storageBucket: "thoughtbank-original-set-up.firebasestorage.app",
+  messagingSenderId: "748757373578",
+  appId: "1:748757373578:web:cd4481637c5c96cad79373",
+  measurementId: "G-XMXJQJTJRN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider
+// const analytics = getAnalytics(app);
+
+//for later use ^^
